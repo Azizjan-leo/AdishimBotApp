de35cce4082fce37297532ab33242876ad7a5ea4
Azizjan Ayupov <litpulla@mail.ru>
2020-11-10 14:28:21 +0600

    .Net Core version has been removed

--- AdishimBotApp/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace AdishimBotApplication.Controllers
-{
-    public class HomeController : Controller
-    {
-        public string Index() => "It's my first Telegram Bot :D";
-    }
-}
